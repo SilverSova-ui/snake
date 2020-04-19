e687fbba78c65018c888681cf82afea4bfd0816b
Sova <mack.kuznetzoff2022@yandex.ru>
2020-04-19 23:09:05 +0300

    Сreature HorizontalLine and VerticalLine

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -11,50 +11,17 @@ namespace Snake
         static void Main(string[] args)
         {
             Point p1 = new Point(1, 3, '*');
+            p1.Draw();
+
 
             Point p2 = new Point(4, 5, '#');
+            p2.Draw();
 
-            Point p3 = new Point(2, 10, '&');
+            HorizontalLine Hline = new HorizontalLine(5,10,8,'+');
+            Hline.Drow();
 
-            Point p4 = new Point(3, 1, '%');
-
-            List<int> numList = new List<int>();
-            numList.Add(0);
-            numList.Add(1);
-            numList.Add(2);
-
-            int x = numList[0];
-            int y = numList[1];
-            int z = numList[2];
-
-            numList.RemoveAt(0);
-
-            List<Point> pList = new List<Point>();
-            pList.Add(p1);
-            pList.Add(p2);
-            pList.Add(p3);
-            pList.Add(p4);
-
-            List<char> pChar = new List<char>();
-            pChar.Add('*');
-            pChar.Add('+');
-            pChar.Add('1');
-
-
-            foreach (var i in pList)
-            {
-                i.Draw();
-            }
-
-            foreach (int q in numList)
-            {
-                Console.Write(q);
-            }
-
-            foreach (char p in pChar)
-            {
-                Console.Write(z);
-            }
+            VerticalLine Vline = new VerticalLine(5, 10, 8, '+');
+            Vline.Drow();
 
             Console.ReadLine();
         }
